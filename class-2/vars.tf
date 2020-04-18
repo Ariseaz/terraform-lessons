102734cfc30e44841d3d6a31bd49f105935adf74
ariseaz <adenijiazeez@gmail.com>
2020-04-18 12:07:53 +0100

    Feat: ami added

--- class-2/vars.tf
+++ class-2/vars.tf
@@ -13,7 +13,7 @@ variable "AMIS" {
   default = {
     us-east-2 = "ami-0fc20dd1da406780b"
     us-west-2 = "ami-0d1cd67c26f5fca19"
-    eu-west-1 = "ami-0d729a60"
+    eu-west-1 = "ami-03ba3948f6c37a4b0"
   }
 }
 
