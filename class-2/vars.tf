d591ab6cf51fe09fe9190dab368b522a0ca03b38
ariseaz <adenijiazeez@gmail.com>
2020-04-18 12:01:57 +0100

    feat: gitignore

--- class-2/vars.tf
+++ class-2/vars.tf
@@ -5,14 +5,14 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
+    us-east-2 = "ami-0fc20dd1da406780b"
+    us-west-2 = "ami-0d1cd67c26f5fca19"
     eu-west-1 = "ami-0d729a60"
   }
 }
