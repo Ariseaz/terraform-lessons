8e04f0e7557c0f1a14f458d37fff4bdafd43f25f
ariseaz <adenijiazeez@gmail.com>
2020-04-18 14:44:45 +0100

    Feat: Software provisioning added

--- software-provisioning/vars.tf
+++ software-provisioning/vars.tf
@@ -5,15 +5,15 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-west-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-2 = "ami-0fc20dd1da406780b"
+    us-west-1 = "ami-03ba3948f6c37a4b0"
+    us-west-2 = "ami-0d1cd67c26f5fca19"
   }
 }
 
