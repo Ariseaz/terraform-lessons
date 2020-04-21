12bd2c8310b8fee4c61859b59a77621dc2aecaf4
ariseaz <adenijiazeez@gmail.com>
2020-04-21 23:09:34 +0100

    Doc: vars.tf updated

--- ec2-in-vpc/vars.tf
+++ ec2-in-vpc/vars.tf
@@ -1,5 +1,20 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-west-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-0fc20dd1da406780b"
+    us-west-1 = "ami-03ba3948f6c37a4b0"
+    us-west-2 = "ami-0d1cd67c26f5fca19"
+  }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -8,14 +23,5 @@ variable "PATH_TO_PRIVATE_KEY" {
 
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
-}
-
-variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
 }
 
