debdf2290b6a6e5198018d9f86613caa0b4512c7
ariseaz <adenijiazeez@gmail.com>
2020-04-28 18:45:01 +0100

    Doc: refactored for s3

--- project_structure_az/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "azbucket-c29df1"
-        key = "terraform-lessons/remote-state"
-        region = "us-west-2"
-    }
-}
--- project_structure_az/dev/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "az-bucket" {
-  bucket = "azbucket-c29df1"
-  acl    = "private"
-
-  tags = {
-    Name = "azbucket-c29df1"
-  }
-}
-
